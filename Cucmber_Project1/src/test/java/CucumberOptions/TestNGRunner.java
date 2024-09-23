b3aedbab5012191403b5f70a7936485141d34cb5
niveditajosh <niveditajoshi27@gmail.com>
2024-09-23 19:08:29 +0530

    Added plugin in TestNGRunner

--- Cucmber_Project1/src/test/java/CucumberOptions/TestNGRunner.java
+++ Cucmber_Project1/src/test/java/CucumberOptions/TestNGRunner.java
@@ -4,7 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/java/FeatureFiles", glue = "StepDefinitions", monochrome = true
+@CucumberOptions(features = "src/test/java/FeatureFiles", glue = "StepDefinitions",
+        monochrome = true, plugin = {"pretty","html:target/cucumber.html","json:target/cucumber.json"}
 )
 public class TestNGRunner extends AbstractTestNGCucumberTests {
 
